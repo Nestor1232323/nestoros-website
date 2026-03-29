cb06c81f2d8d83e4c94451e59d1cddbe3ad01659
Нестор <116899923+Nestor1232323@users.noreply.github.com>
2026-03-29 16:53:31 +0300

    changed system

--- api/v1/system.go
+++ api/v1/system.go
@@ -8,30 +8,40 @@ import (
 )
 
 type ConfigResult struct {
+	Key   string `json:"key,omitempty"`
 	Value string `json:"value"`
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// cors
+	// CORS
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, apikey")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, apikey")
 
-    if r.Method == "OPTIONS" {
-        w.WriteHeader(http.StatusOK)
-        return
-    }
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
-    url := os.Getenv("SUPABASE_URL")
+	url := os.Getenv("SUPABASE_URL")
 	key := os.Getenv("SUPABASE_SERVICE_ROLE_KEY")
 
+	// Получаем значение параметра "get"
 	queryType := r.URL.Query().Get("get")
 	if queryType == "" {
 		queryType = "version"
 	}
 
-	reqURL := fmt.Sprintf("%s/rest/v1/system_config?key=eq.%s&select=value", url, queryType)
+	var reqURL string
 	
+	// Если запрашивают 'all', убираем фильтр по key и запрашиваем оба поля
+	if queryType == "all" {
+		reqURL = fmt.Sprintf("%s/rest/v1/system_config?select=key,value", url)
+	} else {
+		// Для одиночных запросов (version, vercodename, verdesc и т.д.)
+		reqURL = fmt.Sprintf("%s/rest/v1/system_config?key=eq.%s&select=value", url, queryType)
+	}
+
 	req, _ := http.NewRequest("GET", reqURL, nil)
 	req.Header.Set("apikey", key)
 	req.Header.Set("Authorization", "Bearer "+key)
@@ -47,9 +57,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	var results []ConfigResult
 	json.NewDecoder(resp.Body).Decode(&results)
 
-	if len(results) > 0 {
-		fmt.Fprint(w, results[0].Value)
+	// Разделяем логику вывода
+	if queryType == "all" {
+		// Возвращаем полноценный JSON для списка всех настроек
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(results)
 	} else {
-		fmt.Fprint(w, "unknown")
+		// Возвращаем просто строку для обратной совместимости с существующими вызовами
+		if len(results) > 0 {
+			fmt.Fprint(w, results[0].Value)
+		} else {
+			fmt.Fprint(w, "unknown")
+		}
 	}
 }

fa480111b4428432ee2332c34fcd0e60c098ef73
Нестор <116899923+Nestor1232323@users.noreply.github.com>
2026-03-29 12:07:45 +0300

    fix system.go

--- api/v1/system.go
+++ api/v1/system.go
@@ -1,23 +1,25 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
+type ConfigResult struct {
+	Value string `json:"value"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	url := os.Getenv("SUPABASE_URL")
 	key := os.Getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-	// Получаем то, что просит макрос: version, vercodename или verdesc
 	queryType := r.URL.Query().Get("get")
 	if queryType == "" {
-		queryType = "version" // по умолчанию
+		queryType = "version"
 	}
 
-	// Запрос в Supabase к таблице system_config
-	// Используем фильтр по ключу .eq
 	reqURL := fmt.Sprintf("%s/rest/v1/system_config?key=eq.%s&select=value", url, queryType)
 	
 	req, _ := http.NewRequest("GET", reqURL, nil)
@@ -26,25 +28,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if err != nil || resp.StatusCode != 200 {
+	if err != nil {
 		fmt.Fprint(w, "Error")
 		return
 	}
 	defer resp.Body.Close()
 
-	// Supabase REST возвращает массив: [{"value": "8.5"}]
-	// Для простоты макроса вытащим только чистое значение, если нужно.
-	// Но проще всего сделать так:
-	
-	type ConfigResult struct {
-		Value string `json:"value"`
-	}
 	var results []ConfigResult
-	import "encoding/json"
 	json.NewDecoder(resp.Body).Decode(&results)
 
 	if len(results) > 0 {
-		fmt.Fprint(w, results[0].Value) // Отдаем чистый текст для совместимости с VBA
+		fmt.Fprint(w, results[0].Value)
 	} else {
 		fmt.Fprint(w, "unknown")
 	}

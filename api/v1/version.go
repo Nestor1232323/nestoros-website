fcb3d8b28ff00a91026c3b081f147333714ebd88
Нестор <116899923+Nestor1232323@users.noreply.github.com>
2026-03-29 12:57:20 +0300

    add downloads (via bucket)

---
+++ api/v1/version.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	supabaseStorageURL := os.Getenv("SUPABASE_URL") + "/storage/v1/object/public/assets/versions/"
+
+	v := r.URL.Query().Get("v")
+	if v == "" {
+		v = "current"
+	}
+
+	extensions := map[string]string{
+		"5.0b":    ".pptx",
+		"5.0r":    ".pptm",
+		"5.0u1":   ".pptm",
+		"5.0m":    ".pptm",
+		"5.0t":    ".pptm",
+		"5.0tv":   ".pptm",
+		"6.0":     ".pptx",
+		"7.0":     ".pptm",
+		"8.0":     ".pptm",
+		"8.1":     ".pptm",
+		"current": ".pptm",
+	}
+
+	ext, ok := extensions[v]
+	if !ok {
+		http.Error(w, "Version not found", 404)
+		return
+	}
+
+	finalURL := fmt.Sprintf("%s%s%s", supabaseStorageURL, v, ext)
+
+	http.Redirect(w, r, finalURL, http.StatusFound)
+}
